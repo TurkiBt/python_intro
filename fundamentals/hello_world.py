f54805fa28f7eddac34f3929f4372f10c20b4fa7
Turki <turki.fcit@gmail.com>
2021-03-22 14:29:12 -0400

    Hello World

--- fundamentals/hello_world.py
+++ fundamentals/hello_world.py
@@ -1,4 +1,21 @@
-x = "Hello Python"
-print(x)
-y = 42
-print(y)
+# 1. TASK: print "Hello World"
+print("Hello World")
+
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Turki"
+print("Hello ", name)	# with a comma
+print("Hello "+ name)	# with a +
+
+# 3. print "Hello 42!" with the number in a variable
+name = 3
+print("Hello", name)	# with a comma
+#print("Hello" + name)	# with a +	-- this one should give us an error!
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2)) # with .format()
+print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
+
+print(fave_food1.upper())
+print(fave_food1.isdigit())
+print(fave_food2.find('z'))
